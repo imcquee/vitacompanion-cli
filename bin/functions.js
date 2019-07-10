3b0b8116be0e8617c7339b8d41c1d8afbd06f829
Isaac McQueen <mcqueen@Isaacs-MacBook-Pro.local>
2019-07-10 11:58:38 -0400

    pre npm publish

---
+++ bin/functions.js
@@ -0,0 +1,281 @@
+const fs = require('fs');
+const dir = require("path");
+const glob = require("glob");
+const AdmZip = require('adm-zip');
+
+async function launch (ip_addr,target,client,ftp){
+    var found = 0;
+    ftp.connect({host: ip_addr, user: "anonymous", password: "@anonymous", port: 1337})
+    .then(function (serverMessage) {
+    return ftp.cwd("/ux0:/app/");
+    }).then(function () {
+    return ftp.list('/');
+    }).then(async function (res){
+        
+        async function find(){
+            for(i=0; i<res.length; i++){
+                if(res[i].name == target) found = 1;
+            }
+        }
+        await find();
+        async function boot(){
+            if(found){
+                client.start();
+                client.send('launch ' + target + '\n', true);
+            }
+            else console.log("This title is not installed on your device");
+        }
+        await boot();
+    return ftp.end(); 
+    });
+}
+
+function rbt(client){
+    client.start();
+    client.send('reboot\n',true);
+}
+
+function Soff(client) {
+    client.start();
+    client.send('screen off\n',true);
+}
+
+function wake(client) {
+    client.start();
+    client.send('screen on\n', true);
+}
+
+function terminate(client){
+    client.start();
+    client.send('destroy\n', true);
+}
+
+function survive(client, state) {
+    if(!state) {
+        console.log("KEEPING SCREEN ON, PLEASE REMEMBER TO TURN OFF (CTRL-C or close terminal) THIS CAN DEPLETE YOUR BATTERY AND CAUSE SCREEN BURN IN");
+        state = setInterval(function() {
+            
+            client.start();
+            client.send('screen on\n', true);
+        }, 100);
+        return state;
+    }
+    else{
+        console.log("KEEP ALIVE MODE ENDED"); 
+        clearInterval(state); 
+        
+    } 
+    return 0;
+}
+
+
+async function chd (pth) {
+    try {
+        process.chdir(pth);
+    } catch (err) {
+        await console.log("ERROR: Cannot access this directory");
+        process.exit(1);
+    }
+}
+
+async function check_param(fpath) {
+    var eboot = 0;
+    var param = 0;
+    var vpk = 0;
+    result = await fromDir(fpath,"*.{vpk,sfo,bin}");
+    for(i=0;i<result.length;i++){
+        if(dir.extname(result[i]) == '.vpk') vpk = result[i];
+        if(dir.basename(result[i]) == 'eboot.bin') eboot = result[i];
+        if(dir.basename(result[i]) == 'param.sfo') param = result[i];
+    }
+    
+    if(eboot && param) {
+        let fd = fs.openSync(param,'r');
+        
+        var store1 = ["magic", "version", "keyTableOffset", "dataTableOffset" ,"indexTableEntries"];
+        var store2 = ["keyOffset", "param_fmt", "paramLen", "paramMaxLen", "dataOffset"];
+        var param = {};
+        var entry_table = new Array();
+        const rd1 = Buffer.alloc(4);  
+        const rd2 = Buffer.alloc(2);
+        function unpack1 () {
+            for(i=0; i<5; i++){
+                fs.readSync(fd,rd1,0,4);
+                param[store1[i]] = Buffer.from(rd1,0,4).readUInt32LE();
+            }
+        } 
+        await unpack1();
+        if(param["magic"] != 0x46535000) {
+            await console.log("Param.sfo is invalid");
+            process.exit(1);
+        }
+        
+        async function unpack2(){
+            for(i=0; i<param["indexTableEntries"]; i++){
+                var param3 = {}
+                async function unpack3 () {
+                    for(j=0; j<5; j++){
+                        if(j<2) {
+                            fs.readSync(fd,rd2,0,2);
+                            param3[store2[j]] = Buffer.from(rd2,0,2).readUInt16LE();
+                        }
+                        else{
+                            fs.readSync(fd,rd1,0,4);
+                            param3[store2[j]] = Buffer.from(rd1,0,4).readUInt32LE();
+                        }
+
+                    }
+                }
+                await unpack3();
+                entry_table.push(param3);
+            }
+        }
+        await unpack2();
+        
+        let keyTable = await Buffer.alloc(param["dataTableOffset"] - param["keyTableOffset"]);
+        await fs.readSync(fd,keyTable,0, param["dataTableOffset"] - param["keyTableOffset"]);
+        
+        keyTable = await keyTable.toString('utf8');
+        let search, ind, final;
+        for(i=0; i<param["indexTableEntries"]; i++){
+            search = await keyTable.substr(entry_table[i]["keyOffset"]);
+            ind = await search.indexOf("\0");
+            final = await search.substr(0,ind);
+
+            if(final == "TITLE_ID"){
+                TITLE_ID = await Buffer.alloc(entry_table[i]["paramLen"]);
+                await fs.readSync(fd,TITLE_ID, 0,entry_table[i]["paramLen"], param["dataTableOffset"] + entry_table[i]["dataOffset"] - 1);
+                TITLE_ID = await TITLE_ID.toString();
+                var retT = await TITLE_ID.substr(1, entry_table[i]["paramLen"])
+                //IP_cache.put(param,retT);
+                return(retT);
+                
+            }
+        }
+        
+    }
+    else if(vpk){
+        uzip(vpk,fpath);
+        return check_param(fpath);
+        
+    }
+    else{
+        await console.log("ERROR: Make sure eboot.bin and param.sfo, or a vpk is in the directory");
+            process.exit(1)
+    }
+
+}
+
+
+
+async function fpayload(fpath, ip_addr,client,TITLE_ID,ftpDeploy,ftp) {
+    var npath = 0;
+    var ebf = await fromDir(fpath,'eboot.bin');
+    if(ebf) npath = ebf[0]
+    
+    await chd(dir.dirname(npath));
+    
+    var config = {
+        user: "anonymous",
+        password: "@anonymous",
+        host: ip_addr,
+        port: 1337,
+        localRoot: fpath,
+        remoteRoot: "ux0:/app/" + TITLE_ID + "/",
+        include: ["eboot.bin","*.lua"],
+        deleteRemote: true,
+        forcePasv: true
+    }; 
+
+    ftpDeploy
+        .deploy(config)
+        .then(res => console.log("Payload Transferred Successfully"))
+        .catch(err => console.log("ERROR: FTP TRANSFER FAILED"));
+    
+
+    await wake(client);
+    await terminate(client);
+    await launch(ip_addr,TITLE_ID,client,ftp);
+
+}
+
+async function fdeploy(fpath,ip_addr,client,ftpDeploy,ftp) {
+    var TITLE_ID = await check_param(fpath);
+    await fpayload(fpath,ip_addr,client,TITLE_ID,ftpDeploy,ftp);
+}
+
+async function fsend(rRoot,fpath,ip_addr,target,ftpDeploy) {
+    var config = {
+        user: "anonymous",
+        password: "@anonymous",
+        host: ip_addr,
+        port: 1337,
+        localRoot: fpath,
+        remoteRoot: rRoot,
+        include: [target],
+        deleteRemote: true,
+        forcePasv: true
+    }; 
+
+    ftpDeploy
+        .deploy(config)
+        .then(res => console.log("Target Transferred Successfully"))
+        .catch(err => console.log("ERROR: FTP TRANSFER FAILED"));
+
+}
+
+async function fromDir(fpath,filter){
+    if(fs.existsSync(fpath)) {
+        await chd(fpath);
+        var npath = 0;
+        var start = "**/" + filter;
+        var ebf = glob.sync(start, {realpath: true, root: fpath, stat: true});
+        if(ebf) {
+            return ebf;
+        }
+        else{
+            await console.log("ERROR: Make sure eboot.bin and param.sfo are in the directory");
+            return null;
+        }
+    }
+    else{
+        await console.log("ERROR: Path not found");
+        return null;
+    }  
+};
+
+async function deb(fpath,ip_addr,client,ftpDeploy,ftp) {
+    var result = await fromDir(fpath,'eboot.bin');
+    var eboot = 0;
+    for(i=0;i<result.length;i++){
+        if(dir.basename(result[i]) == 'eboot.bin') eboot = result[i];
+    }
+    eboot = eboot.toString();
+    fdeploy(fpath,ip_addr,client,ftpDeploy,ftp); 
+    if(eboot)return dir.dirname(eboot);
+    else return 0;
+};
+
+async function uzip(vpk,fpath){
+    var zip = new AdmZip(vpk);
+    await zip.extractAllTo(fpath+"/tempV");
+    
+}
+
+
+
+module.exports = {
+    launch,
+    wake,
+    terminate,
+    check_param,
+    fpayload,
+    fsend,
+    survive,
+    chd,
+    fromDir,
+    fdeploy,
+    deb,
+    rbt,
+    Soff,
+}
---
+++ bin/vitacompanion-cli.js
@@ -0,0 +1,144 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+const fs = require('fs');
+const FtpDeploy = require('ftp-deploy');
+const PromiseFtp = require('promise-ftp');
+const Netcat = require('node-netcat');
+const chokidar = require('chokidar');
+const utils = require('./functions');
+var settings = './settings.json';
+var save = require(settings);
+
+var client = new Netcat.client(1338, save.ip_addr, {timeout: 5000});
+var ftpDeploy = new FtpDeploy();
+var ftp = new PromiseFtp();
+
+program
+ .version('1.0.0','-v, --version')
+ .description('A Command Line Interface for communicating with the Vita via devnonam120 vitacompanion library') 
+
+program
+ .command('ip <ip_addr>')
+ .description('Set Device IP Address')
+ .action(function (ip_addr) {
+   console.log(ip_addr);
+   save.ip_addr = ip_addr;
+   console.log(__dirname);
+   fs.writeFileSync(__dirname + '/settings.json',JSON.stringify(save));
+})
+
+program
+  .command('on')
+  .description('Turn Screen On')
+  .action(function () {
+    if(!save.ip_addr) {
+      console.log('Connect to the Vita first using command: vita ip <ip address> ')
+    }
+    else{
+      utils.wake(client);
+    }
+})
+
+program
+  .command('off')
+  .description('Turn Screen Off')
+  .action(function () {
+    if(!save.ip_addr) {
+      console.log('Connect to the Vita first using command: vita ip <ip address> ')
+    }
+    else{
+      utils.Soff(client);
+    }
+})
+
+program
+  .command('reboot')
+  .alias('r')
+  .description('Reboot Device')
+  .action(function () {
+    if(!save.ip_addr) {
+      console.log('Connect to the Vita first using command: vita ip <ip address> ')
+    }
+    else{
+      utils.rbt(client);
+    }
+})
+
+program
+  .command('launch <TITLE_ID>')
+  .alias('l')
+  .description('Launch Application by ID')
+  .action(function (TITLE_ID) {
+    utils.launch(save.ip_addr,TITLE_ID,client,ftp);
+    console.log('Attempting to launch application' + TITLE_ID);
+})
+
+program
+  .command('kill')
+  .alias('k')
+  .description('Kill all Running Applications')
+  .action(function () {
+    if(!save.ip_addr) {
+      console.log('Connect to the Vita first using command: vita ip <ip address> ')
+    }
+    else{
+      utils.terminate(client);
+    }
+})
+
+program
+  .command('copy <target> <dir>')
+  .alias('cp')
+  .description('Copy Local File to Device')
+  .action(function (target, dir) {
+    if(!save.ip_addr) {
+      console.log('Connect to the Vita first using command: vita ip <ip address> ')
+    }
+    else{
+
+      utils.fsend(dir,process.cwd(),save.ip_addr,target,ftpDeploy)
+    }
+    console.log('Attempting to send file:' + target);
+})
+
+program
+  .command('payload')
+  .alias('p')
+  .description('Send and Run Payload')
+  .action(function () {
+    utils.fdeploy(process.cwd(),save.ip_addr,client,ftpDeploy,ftp);
+})
+
+program
+  .command('stay')
+  .alias('s')
+  .description('Toggle Keep Screen On Mode') 
+  .action(function () {
+    save.SMODE = utils.survive(client,save.SMODE);
+})
+
+program
+  .command('debug')
+  .alias('d')
+  .description('Toggle Debug Mode')
+  .action(async function () {
+    save.SMODE = utils.survive(client,0);
+    console.log("Debug Mode: Started");
+    var eboot = await utils.deb(process.cwd(),save.ip_addr,client,ftpDeploy,ftp);
+    if(eboot) utils.chd(eboot);
+
+    watcher = chokidar.watch('eboot.bin').on('change', (event, path) => {
+        utils.fdeploy(process.cwd(),save.ip_addr,client,ftpDeploy,ftp); 
+    });
+})
+
+client.on('data', function (data) {
+  if(!save.SMODE) console.log('Received: ' + data); 
+})
+
+client.on('error', function (err) {
+  if(!save.SMODE) console.log('ERROR: Connection Failed'); 
+})
+
+program.parse(process.argv);
